b221ffde8b3c12e416ece7ee3caf60d144524fe1
Ukendiran <ukendiran@gmail.com>
2022-07-03 19:54:24 +0530

    background-check

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World</title>
+    <style>
+        body {
+            background-color: #237367;
+        }
+    </style>
 </head>
 
 <body>
